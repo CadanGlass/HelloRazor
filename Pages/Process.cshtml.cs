a3633a02b15556515e1c6243e626219c4e3245dd
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-09-03 14:03:49 -0700

    Fix bug

--- Pages/Process.cshtml.cs
+++ Pages/Process.cshtml.cs
@@ -1,15 +1,13 @@
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
-
 
 namespace HelloRazor.Pages
 {
     public class ProcessModel : PageModel 
     {
         public void OnGet() {
-            var Procs = Process.GetCurrentProcess();
-            ViewData["Procs"] = Procs;
+            var procs = Process.GetProcesses();  // Get all processes
+            ViewData["Procs"] = procs;           // Store them in ViewData
         }
     }
 }
